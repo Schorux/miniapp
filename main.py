b412c31c5e11b605bc7033b89e8e8e3e546d3ffd
Schorux <maxachkalinskiy66@gmail.com>
2026-06-26 10:52:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,30 @@
 """
 import asyncio
 import logging
+import sys
+import os
+
+sys.path.insert(0, '/app')
+os.chdir('/app')
+
 import uvicorn
 from webapp import app as fastapi_app
-from bot import main as bot_main
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+async def run_bot():
+    from bot import main as bot_main
+    await asyncio.to_thread(bot_main)
+
 async def run_all():
     config = uvicorn.Config(fastapi_app, host="0.0.0.0", port=8000, log_level="warning")
     server = uvicorn.Server(config)
     await asyncio.gather(
         server.serve(),
-        asyncio.to_thread(bot_main)
+        run_bot()
     )
 
 if __name__ == '__main__':
